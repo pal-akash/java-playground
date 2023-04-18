f7b1ac3a695216a37acb388ceaea453bf59dbd51
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2023-04-19 00:19:19 +0530

    2. contact-app: deleted old app

--- 2. contact app/ContactFeature.java
+++
@@ -1,6 +0,0 @@
-interface ContactInfo{
-    public String getData();
-    public void setData(String detail);
-    public String getLabel();
-    public void setLabel(String label);
-}
--- 2. contact app/Main.java
+++
@@ -1,7 +0,0 @@
-
-public class Main {
-    public static void main(String[] args) {
-        App appObj = new App();
-        appObj.runApp();
-    }
-}
