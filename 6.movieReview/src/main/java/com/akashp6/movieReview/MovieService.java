7044c4da01368b0e0abc69fa89701ae06e1816da
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2023-06-04 15:06:41 +0530

    6.movie-review: [feat] get all movies

---
+++ 6.movieReview/src/main/java/com/akashp6/movieReview/MovieService.java
@@ -0,0 +1,19 @@
+package com.akashp6.movieReview;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public List<Movie> allMovies(){
+        return movieRepository.findAll();
+    }
+}
