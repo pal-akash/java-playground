a2a366d6559a83a8fab88cce7ce08b865f946aae
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2023-06-05 22:59:33 +0530

    6.movie-review: [feat] post review

---
+++ 6.movieReview/src/main/java/com/akashp6/movieReview/ReviewRepository.java
@@ -0,0 +1,11 @@
+package com.akashp6.movieReview;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
