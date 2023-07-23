43cf7f7650292a494026502f68555184d74498ca
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2023-07-23 11:11:26 +0530

    [feat] delete a review

--- 6.movieReview/src/main/java/com/akashp6/movieReview/ReviewController.java
+++ 6.movieReview/src/main/java/com/akashp6/movieReview/ReviewController.java
@@ -1,12 +1,10 @@
 package com.akashp6.movieReview;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -21,4 +19,11 @@ public class ReviewController {
     public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payLoad){
         return new ResponseEntity<Review>(reviewService.createReview(payLoad.get("reviewBody"), payLoad.get("imdbId")), HttpStatus.CREATED);
     }
+
+    @RequestMapping("/deletereview")
+    public ResponseEntity<?> deleteReview(@RequestParam("id") ObjectId reviewId){
+        return new ResponseEntity<String>(reviewService.deleteReview(reviewId), HttpStatus.OK);
+    }
+
+
 }
--- 6.movieReview/src/main/java/com/akashp6/movieReview/ReviewService.java
+++ 6.movieReview/src/main/java/com/akashp6/movieReview/ReviewService.java
@@ -1,10 +1,15 @@
 package com.akashp6.movieReview;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.UUID;
 
 @Service
 public class ReviewService {
@@ -25,4 +30,16 @@ public class ReviewService {
 
         return review;
     }
+
+//    private String getRandomIdentifier() {
+//        return UUID.randomUUID().toString();
+//    }
+
+    public String deleteReview(ObjectId objectId){
+
+        reviewRepository.deleteById(objectId);
+
+        return "Review deleted successfully";
+    }
+
 }
