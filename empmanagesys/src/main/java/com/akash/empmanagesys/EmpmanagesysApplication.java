88ae095d55ea5ca5132a031906fcc8fbe485af84
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2023-07-23 21:59:45 +0530

    [init] initialize project

---
+++ empmanagesys/src/main/java/com/akash/empmanagesys/EmpmanagesysApplication.java
@@ -0,0 +1,13 @@
+package com.akash.empmanagesys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpmanagesysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpmanagesysApplication.class, args);
+	}
+
+}
---
+++ empmanagesys/src/test/java/com/akash/empmanagesys/EmpmanagesysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.empmanagesys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpmanagesysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
